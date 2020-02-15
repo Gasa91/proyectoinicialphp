d906181c77a529b055d4d12b1efc3f208b3d1af1
unknown <yoanysanchez1212@gmail.com>
2020-02-14 21:20:34 -0500

    esta es la versión fucinal

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//esto es un comentario de una sola linea 
+/*esto es un comentario de varias lineas*/
+
+//asi se declara las varaivles en php $
+
+$nombre="Giovanny"; 
+echo ($nombre);
+$edad=29;
+echo ($edad);
+$estatura=1.69;
+echo ($estatura);
+
+echo("hola mundo");
+print("<br>");
+echo ("mi nombre es ".$nombre . " y la edad es ".$edad);
+
+
+
+
+
+
+
+
+
+
+
+
+?>
